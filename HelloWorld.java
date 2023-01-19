ef7d7874f2ba8cc8dda5a71d1c45afd928ec09da
xavier <if720960@iteso.mx>
2023-01-19 11:26:10 -0600

    Commit1
    add the 1srt java class

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld { 
+    
+    /* 
+    nombre de clase igual al archivo  
+    */
+
+    public static void main (String[] args) { //Este método es fundamental para que el programa se ejecute
+
+        System.out.println("Hola Mundo desde Java"); //Las instrucciones terminan con ;
+    } // fin del método 
+}
+
+//Las clases se escriben con mayusculas y las variables con minusculas.
+// void tipo de retorno, este metodo no regresa
